298adf50a676508729eaf6c1c9745aeb343e2d1b
berhanu-tarekegn <beau.tarek@gmail.com>
2025-08-12 15:27:54 +0300

    Initial commit

---
+++ src/test/java/com/kifiya/PromoQuoter/PromoQuoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kifiya.PromoQuoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoQuoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
